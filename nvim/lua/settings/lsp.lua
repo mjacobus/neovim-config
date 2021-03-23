7127e6f1dffa900f740ffbe10dd640430e9328e7
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-03-23 01:33:25 -0300

    Improve autocomplete config

--- nvim/lua/settings/lsp.lua
+++ nvim/lua/settings/lsp.lua
@@ -1,9 +1,17 @@
-completion = require('completion').on_attach
 config = require('lspconfig')
+completion = require('completion').on_attach
+inoremap = require('utils').inoremap
 
 config.solargraph.setup{ on_attach = completion }
 config.tsserver.setup{ on_attach = completion }
 
---Set completeopt to have a better completion experience
+config.sumneko_lua.setup{}
+
+-- Autocomplete
 vim.api.nvim_command('set completeopt=menuone,noinsert,noselect')
 vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
+
+
+inoremap('<tab>', '<Plug>(completion_smart_tab)') -- conflicts with  ultisnippets
+inoremap('<s-tab>', '<Plug>(completion_smart_s_tab)')
+vim.api.nvim_exec('imap <silent> <c-p> <Plug>(completion_trigger)', true)
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -2,6 +2,11 @@ local nnoremap = function(mapping, command)
   vim.api.nvim_set_keymap('n', mapping, command, { noremap = true })
 end
 
+local inoremap = function(mapping, command)
+  vim.api.nvim_set_keymap('i', mapping, command, { noremap = true })
+end
+
 return {
-  nnoremap = nnoremap
+  nnoremap = nnoremap,
+  inoremap = inoremap,
 }
